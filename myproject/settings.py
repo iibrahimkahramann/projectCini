5cc80e583658bdac71ff6e554186ced5d98ddc79
İbrahim Kahraman <ikahraman1938@gmail.com>
2023-08-23 00:27:55 +0300

    app yayınlanmak için hazır

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,10 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'assets/static/'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'assets/static'
 ]
+
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
